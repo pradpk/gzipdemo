2935cd21d409980392a835a1d3f11e140819809c
unknown <pradhip.nit@gmail.com>
2020-05-11 16:22:40 -0400

    trying out deployment in openshift

---
+++ src/main/java/com/pradpk/gzipdemo/GzipdemoApplication.java
@@ -0,0 +1,13 @@
+package com.pradpk.gzipdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GzipdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GzipdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pradpk/gzipdemo/GzipdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradpk.gzipdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GzipdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
